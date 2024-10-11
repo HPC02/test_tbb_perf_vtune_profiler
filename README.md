4cbb5ad176dfba01bc2acf7c20c8f6d49bbea3c4
hxf0223 <hxf0223@hotmail.com>
2024-10-11 17:35:27 +0800

    setup demo project

---
+++ README.md
@@ -0,0 +1,3 @@
+# README #
+
+测试使用 `VTune Profiler` 进行性能分析：[使用VTune Profiler测试TBB overhead](https://www.intel.com/content/www/us/en/docs/vtune-profiler/cookbook/2024-1/intel-tbb-scheduling-overhead.html)。
---
+++ src/main.cc
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <chrono>
+#include <iostream>
+
+#include <tbb/tbb.h>
+
+namespace {
+
+const size_t SIZE = 50 * 1000 * 1000;
+double v[SIZE];
+
+}  // namespace
+
+void VectorInit(double* v, size_t n) {
+  tbb::parallel_for(size_t(0), n, size_t(1), [=](size_t i) { v[i] = i * 2; });
+}
+
+double VectorReduction(double* v, size_t n) {
+  return tbb::parallel_deterministic_reduce(
+    tbb::blocked_range<double*>(v, v + n), 0.f,
+    [](const tbb::blocked_range<double*>& r, double value) -> double { return std::accumulate(r.begin(), r.end(), value); },
+    std::plus<double>());
+}
+
+int main(int argc, char* argv[]) {
+  const auto t0 = std::chrono::high_resolution_clock::now();
+  //task_scheduler_init(task_scheduler_init::automatic);
+  VectorInit(v, SIZE);
+
+  double sum{};
+  for (int i = 0; i < 100; i++) {
+    sum = VectorReduction(v, SIZE);
+  }
+
+  const auto t1 = std::chrono::high_resolution_clock::now();
+  const auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count();
+  std::cout << "Duration: " << duration / 1000.0 << " seconds" << std::endl;
+
+  return 0;
+}
